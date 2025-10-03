bb1ac90cf957d4c827ee161137c2755055e72338
AnnaBatmaeva <anabatmaeva@gmail.com>
2025-10-03 22:08:07 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+
+import cats from './assets/images/1.jpg';
+
+const img = document.createElement('img')
+img.src = cats;
+document.body.appendChild(img)
+
+console.log('Hello!')
+console.log('World')
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const { title } = require('process')
+const { Template } = require('webpack')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        filename: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+        assetModuleFilename: 'images/[name].[contenthash][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.[contenthash].css'
+        }
+        )
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 8 * 1024
+                    },
+
+                },
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        }
+                    }
+                ],
+
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin(),
+        ],
+        minimize: true,
+    },
+};
